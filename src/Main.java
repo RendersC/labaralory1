c4602004192c9b4d8c603c28bd5bd58be1ec0f08
RendersC <crewsrenders@gmail.com>
2025-03-28 14:39:48 +0500

    added second problem

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
         //testing problem1
         int[] arr = {3, 1, 4, 1, 5, 9, 2, 6};
         System.out.println(problem1(8,arr)+" | problem1 results");
+        System.out.println(problem2(8,arr)+" | problem2 results");
     }
+
 
     /*
     *In this problem we find minimum value in array
@@ -20,5 +22,20 @@ public class Main {
     }
 
 
+    /*
+    *Finds the average value of an array
+    * Time complexity O(n)
+     */
+    public static float problem2(int n,int[] arr) {
+        float avg = 0;
+        for (int i = 0; i < n; i++) {
+            avg += arr[i];
+        }
+        return avg/n;
+    }
 
+
+    public static String problem3(int n) {
+        
+    }
 }
